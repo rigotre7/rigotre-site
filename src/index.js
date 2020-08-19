6fbb4f60d5b2032bcc5876b296b52273e5c0772e
Rodrigo Trejo <rigotre7@gmail.com>
2020-08-19 15:29:19 -0400

    fix css imports

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+import './css/custom.scss';
 
 ReactDOM.render(
   <React.StrictMode>
