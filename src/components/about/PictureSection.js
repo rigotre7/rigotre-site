adf2e7590251fb267d3e1abf9a606e638408e87d
Rodrigo Trejo <rigotre7@gmail.com>
2020-08-20 12:38:15 -0400

    change where images are imported

--- src/components/about/PictureSection.js
+++ src/components/about/PictureSection.js
@@ -4,35 +4,21 @@ import PictureViewer from "./../about/PictureViewer";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
-const importAll = (r) => {
-    return r.keys().sort((a, b) => {
-        // Sort the images by name
-        const regex = /(?<=\/)(.*?)(?=\.)/g;
-        const numA = Number(a.match(regex)[0]);
-        const numB = Number(b.match(regex)[0]);
-
-        return numA - numB;
-    }).map(r);
-}
-
-const thumbnails = importAll(require.context('./../../assets/about/thumbnails', false, /\.(png|jpe?g|svg)$/));
-const fullImages = importAll(require.context('./../../assets/about/full', false, /\.(png|jpe?g|svg)$/));
-
-const PictureSection = () => {
+const PictureSection = (props) => {
     return (
-        <div style={{height: "calc(100vh - 56px)"}}>
+        <React.Fragment>
             <Container>
                 <Row className="justify-content-center picture-viewer-desc">
-                    <h1>
-                        Peek Into My Life
-                    </h1>
+                    <h2 style={{textAlign: "center"}}>
+                        Some of my favorite places
+                    </h2>
                 </Row>
             </Container>
             <PictureViewer
-                thumbnails={thumbnails}
-                fullImages={fullImages}
+                thumbnails={props.thumbnails}
+                fullImages={props.fullImages}
             />
-        </div>
+        </React.Fragment>
     );
 }
 
